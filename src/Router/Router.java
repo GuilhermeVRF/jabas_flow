0f1202df2aef0b7a98e83c497037922ced3825c1
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-01-11 17:13:12 -0300

    feat: cria arquivo de roteamento de views

---
+++ src/Router/Router.java
@@ -0,0 +1,38 @@
+package Router;
+import java.sql.Connection;
+
+import javax.swing.JPanel;
+
+import controllers.UserController;
+import database.MySQLConnection;
+import models.User;
+import repositories.UserRepository;
+import services.UserService;
+import utils.Response;
+import views.Main;
+
+public final class Router {
+	/* Displaying view */
+	public static JPanel displayingView = null;
+	
+	public static void displayView(JPanel view) {
+		if(displayingView != null) Main.FRAME.remove(displayingView);
+		
+		displayingView = view;
+		Main.FRAME.add(view);
+		Main.FRAME.revalidate();
+		Main.FRAME.repaint();
+	}
+	
+	/* Database Connection */
+	private static final Connection connection = MySQLConnection.getConnection();
+	
+	/* User handler */
+	private static final UserRepository USER_REPOSITORY = new UserRepository(connection);
+	private static final UserService USER_SERVICE = new UserService(USER_REPOSITORY);
+	private static final UserController USER_CONTROLLER = new UserController(USER_SERVICE);
+	
+	public static Response storeUser(User user) {
+		return USER_CONTROLLER.store(user);
+	}
+}
