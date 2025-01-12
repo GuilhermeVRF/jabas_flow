90e174d98048cd41a2478df37126eb05c4bf6c11
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-01-12 16:21:33 -0300

    erfactor: adiciona metodo de posicionamento de elementos em um painel

--- src/views/BasePanel.java
+++ src/views/BasePanel.java
@@ -2,18 +2,43 @@ package views;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 
 import javax.swing.JPanel;
 
 import views.components.Header;
 
 public abstract class BasePanel extends JPanel{
+	protected final GridBagConstraints FLEX_COLUMN = new GridBagConstraints();
+	
 	public BasePanel() {
 		this.setLayout(new BorderLayout());
 		this.setPreferredSize(new Dimension(100,100));
-		
+		this.makeResponsive();
 		this.add(new Header(), BorderLayout.NORTH);
 	}
 	
 	protected abstract void createBody();
+	
+	protected void justifyContent(JPanel component, String position) {
+		Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		component.setMinimumSize(new Dimension(500, 300));
+		
+		switch(position) {
+			case "middle": 
+				Dimension middleScreen = new Dimension(screenSize.width / 2, screenSize.height / 2);
+				component.setPreferredSize(middleScreen);
+				component.setMaximumSize(middleScreen);
+				FLEX_COLUMN.anchor = GridBagConstraints.CENTER;
+			break;
+		}
+	}
+	
+	private void makeResponsive() {
+		FLEX_COLUMN.gridx = 0;
+		FLEX_COLUMN.gridy = 0;
+		FLEX_COLUMN.weightx = 1.0;
+		FLEX_COLUMN.weighty = 1.0; 
+		FLEX_COLUMN.fill = GridBagConstraints.NONE; 
+	}
 }
--- src/views/Register.java
+++ src/views/Register.java
@@ -25,25 +25,10 @@ public class Register extends BasePanel{
 		
 		RegisterForm registerForm = new RegisterForm();
 
-        Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        Dimension middleScreen = new Dimension(screenSize.width / 2, screenSize.height / 2);
-        registerForm.setPreferredSize(middleScreen);
-        
-        // Configura o layout para centralizar e permitir redimensionamento
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0; 
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.fill = GridBagConstraints.NONE; 
-
-        // Define tamanhos mínimos e máximos para o RegisterForm
-        registerForm.setMinimumSize(new Dimension(500, 300));
-        registerForm.setMaximumSize(middleScreen);
+        this.justifyContent(registerForm, "middle");
 
         // Adiciona o formulário ao painel
-        body.add(registerForm, gbc);
+        body.add(registerForm, FLEX_COLUMN);
 
         
 		body.setBackground(new Color(0xF4F6FF));
