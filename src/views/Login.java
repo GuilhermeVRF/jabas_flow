b31afaf6f6a66591931bb8d405742800c574cc5d
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-01-12 16:22:28 -0300

    feat: cria a view de Login

--- src/views/Login.java
+++ src/views/Login.java
@@ -1,9 +1,34 @@
 package views;
 
-public class Login extends BasePanel{
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
 
+import javax.swing.JPanel;
+
+import views.forms.LoginForm;
+
+public class Login extends BasePanel{
+	public Login() {
+		super();
+		this.setVisible(true);
+		
+		this.createBody();
+	}
+	
 	@Override
 	protected void createBody() {	
+		JPanel body = new JPanel();
+		body.setLayout(new GridBagLayout());
+		body.setPreferredSize(new Dimension(100, 100));
+		
+		LoginForm loginForm = new LoginForm();
+		this.justifyContent(loginForm, "middle");
+		body.add(loginForm);
+		
+		body.setBackground(new Color(0xF4F6FF));
+		this.add(body);
+		body.setVisible(true);
 	}
 
 }
---
+++ src/views/forms/LoginForm.java
@@ -0,0 +1,75 @@
+package views.forms;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import Router.Router;
+import requests.LoginRequest;
+import views.Register;
+import views.customs.Label;
+import views.customs.PasswordField;
+import views.customs.RoundedBorder;
+import views.customs.TextField;
+
+public class LoginForm extends Form{
+	public LoginForm() {
+		this.setOpaque(false);
+        this.setBackground(new Color(0,0,0));
+        this.setBorder(new RoundedBorder(30));
+        this.setVisible(true);
+        
+        this.createFormFields();
+	}
+	@Override
+	protected void createFormFields() {
+		this.createTitle("Login");
+		
+        JTextField emailField = new TextField();
+        this.createField("E-mail", emailField, 1);
+		
+        PasswordField passwordField = new PasswordField();
+        this.createPasswordField("Senha", passwordField, 2);
+        
+        this.createSubmitButton("Enviar", 3, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String email = emailField.getText();
+				String password = new String(passwordField.getPassword());
+				LoginRequest loginRequest = new LoginRequest(email, password);
+			}
+		});
+        
+        JLabel dontHaveAccount = new Label("<html><u>Não possui cadastro? Crie sua conta!</u></html>");
+        dontHaveAccount.setForeground(Color.BLUE);
+        FLEX_COLUMN.gridx = 0;FLEX_COLUMN.gridy = 7;FLEX_COLUMN.gridwidth = 2; 
+        FLEX_COLUMN.anchor = GridBagConstraints.LINE_START;
+        this.add(dontHaveAccount, FLEX_COLUMN);
+        
+        dontHaveAccount.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Router.displayView(new Register());
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            	dontHaveAccount.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            	dontHaveAccount.setCursor(Cursor.getDefaultCursor());
+            }
+        });
+	}
+
+}
