cc627178c97ddeaf3f4f4080fc29c4f6de0455b7
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-01-12 16:01:33 -0300

    refactor: cria metodos de grupos de formulários na classe Form

--- src/views/forms/Form.java
+++ src/views/forms/Form.java
@@ -1,14 +1,64 @@
 package views.forms;
 
-import javax.swing.BoxLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+
+import views.customs.Label;
+import views.customs.PasswordField;
+import views.customs.RoundedBorder;
 
 public abstract class Form extends JPanel{
+	protected final Font TITLE_FONT = new Font("Roboto", Font.BOLD, 30);
+	protected final GridBagConstraints FLEX_COLUMN = new GridBagConstraints();
 	
 	public Form() {
-		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
+		this.setOpaque(false);
+        this.setBackground(new Color(0,0,0));
+        this.setBorder(new RoundedBorder(30));
+        this.setLayout(new GridBagLayout());
+        
+        this.FLEX_COLUMN.insets = new Insets(10, 10, 10, 10);
 	}
 	
 	protected abstract void createFormFields();
+	
+	protected void createTitle(String titleText) {
+	    JLabel titleLabel = new JLabel(titleText);
+	    titleLabel.setFont(this.TITLE_FONT);  
+	    FLEX_COLUMN.gridx = 0;
+	    FLEX_COLUMN.gridy = 0;
+	    FLEX_COLUMN.gridwidth = 2;
+	    FLEX_COLUMN.anchor = GridBagConstraints.CENTER;  
+	    
+	    this.add(titleLabel, FLEX_COLUMN);
+	    
+	    FLEX_COLUMN.gridwidth = 1;
+	    FLEX_COLUMN.anchor = GridBagConstraints.WEST;
+	}
+	
+	protected void createField(String labelText, JTextField field, int gridy) {
+        Label label = new Label(labelText); 
+        FLEX_COLUMN.gridx = 0; 
+        FLEX_COLUMN.gridy = gridy; 
+        this.add(label, FLEX_COLUMN); 
+        
+        FLEX_COLUMN.gridx = 1; 
+        this.add(field, FLEX_COLUMN); 
+    }
+	
+	protected void createPasswordField(String labelText, PasswordField passwordField, int gridy) {
+	    this.createField(labelText, passwordField, gridy);
+
+	    JToggleButton togglePassword = passwordField.getToggleButton();
+	    FLEX_COLUMN.gridx = 2;
+	    this.add(togglePassword, FLEX_COLUMN);
+	}
 }
--- src/views/forms/RegisterForm.java
+++ src/views/forms/RegisterForm.java
@@ -1,124 +1,104 @@
 package views.forms;
 
 import java.awt.Color;
-import java.awt.Font;
+import java.awt.Cursor;
 import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JToggleButton;
-
 import Router.Router;
 import requests.UserRequest;
 import utils.Response;
+import views.Login;
 import views.customs.Button;
 import views.customs.FileInput;
 import views.customs.Label;
 import views.customs.PasswordField;
-import views.customs.RoundedBorder;
 import views.customs.TextField;
 
-public class RegisterForm extends Form {
+public final class RegisterForm extends Form {
     public RegisterForm() {
-        this.setOpaque(false);
-        this.setBackground(new Color(0,0,0));
-        this.setBorder(new RoundedBorder(30));
         createFormFields();  
     }
 
     protected void createFormFields() {
-        this.setLayout(new GridBagLayout());
-        
-        GridBagConstraints gbc = new GridBagConstraints();
-        
-        JLabel registerTitle = new JLabel("Registrar");
-        registerTitle.setFont(new Font("Roboto", Font.BOLD, 30));
-        gbc.gridx = 0; gbc.gridy = 0; 
-        gbc.gridwidth = 2;           
-        gbc.anchor = GridBagConstraints.CENTER;   
-        this.add(registerTitle, gbc);
-        
-        gbc.insets = new Insets(10, 10, 10, 10);
-        gbc.anchor = GridBagConstraints.WEST;
-
-        gbc.gridwidth = 1; 
-        gbc.insets = new Insets(10, 10, 10, 10);
-        gbc.anchor = GridBagConstraints.WEST;
-        
+    	this.createTitle("Registrar");
+ 
         // Painel de Nome
-        JLabel nameLabel = new Label("Nome");
         JTextField nameField = new TextField();
-        gbc.gridx = 0; gbc.gridy = 1; 
-        this.add(nameLabel, gbc);
-        gbc.gridx = 1; 
-        this.add(nameField, gbc);
+        this.createField("Nome", nameField, 1);
 
         // Painel de E-mail
-        JLabel emailLabel = new Label("E-mail");
         JTextField emailField = new TextField();
-        gbc.gridx = 0; gbc.gridy = 2;
-        this.add(emailLabel, gbc);
-        gbc.gridx = 1;
-        this.add(emailField, gbc);
+        this.createField("E-mail", emailField, 2);
 
         // Painel de Senha
-        JLabel passwordLabel = new Label("Senha");
         PasswordField passwordField = new PasswordField();
-        JToggleButton togglePassword = passwordField.getToggleButton();
-        gbc.gridx = 0; gbc.gridy = 3;
-        this.add(passwordLabel, gbc);
-        gbc.gridx = 1;
-        this.add(passwordField, gbc);
-        gbc.gridx = 2;
-        this.add(togglePassword, gbc);
+        this.createPasswordField("Senha", passwordField, 3);
 
         // Painel de Repita a Senha
-        JLabel repeatPasswordLabel = new Label("Repita a Senha");
         PasswordField repeatPasswordField = new PasswordField();
-        JToggleButton toggleRepeatPassword = repeatPasswordField.getToggleButton();
-        gbc.gridx = 0; gbc.gridy = 4;
-        this.add(repeatPasswordLabel, gbc);
-        gbc.gridx = 1;
-        this.add(repeatPasswordField, gbc);
-        gbc.gridx = 2;
-        this.add(toggleRepeatPassword, gbc);
+        this.createPasswordField("Repita a Senha", repeatPasswordField, 4);
         
         JLabel profileFileLabel = new Label("Imagem de perfil");
         FileInput profileFileField = new FileInput("Selecionar imagem");
-        gbc.gridx = 0; gbc.gridy = 5;
-        this.add(profileFileLabel, gbc);
-        gbc.gridx = 1;
-        this.add(profileFileField, gbc);
+        FLEX_COLUMN.gridx = 0; FLEX_COLUMN.gridy = 5;
+        this.add(profileFileLabel, FLEX_COLUMN);
+        FLEX_COLUMN.gridx = 1;
+        this.add(profileFileField, FLEX_COLUMN);
         
         // Botão de Enviar
         JButton submitBtn = new Button("Enviar");
-        gbc.gridx = 0; gbc.gridy = 6; gbc.gridwidth = 2; // O botão ocupa duas colunas
-        gbc.anchor = GridBagConstraints.CENTER;         // Centraliza o botão
-        this.add(submitBtn, gbc);
+        FLEX_COLUMN.gridx = 0; FLEX_COLUMN.gridy = 6; FLEX_COLUMN.gridwidth = 2; 
+        FLEX_COLUMN.anchor = GridBagConstraints.CENTER;      
+        this.add(submitBtn, FLEX_COLUMN);
         
         submitBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 	            String username = nameField.getText();
 	            String email = emailField.getText();
-	            String password = passwordField.getText();
-	            String repeatPassword = repeatPasswordField.getText();
+	            String password = new String(passwordField.getPassword());
+	            String repeatPassword = new String(repeatPasswordField.getPassword());
 	            
 	            UserRequest userRequest = new UserRequest(username, email, password, repeatPassword, profileFileField.getSelectedFile());
 	            Response response = Router.storeUser(userRequest);
-	            System.out.println(response.getSTATUS());
+	            
 	            if(response.getSTATUS().equals("success")) {
 	            	JOptionPane.showMessageDialog(null, response.getMESSAGE(), "Sucesso!", JOptionPane.INFORMATION_MESSAGE);
 	            }else {
 	            	JOptionPane.showMessageDialog(null, response.getMESSAGE(), "Error", JOptionPane.ERROR_MESSAGE);
 	            }
+            }
+        });
+        
+        JLabel alreadyRegistred = new Label("<html><u>Já possui cadastro? Faça login!</u></html>");
+        alreadyRegistred.setForeground(Color.BLUE);
+        FLEX_COLUMN.gridx = 0;FLEX_COLUMN.gridy = 7;FLEX_COLUMN.gridwidth = 2; 
+        FLEX_COLUMN.anchor = GridBagConstraints.LINE_START;
+        this.add(alreadyRegistred, FLEX_COLUMN);
+        
+        alreadyRegistred.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Router.displayView(new Login());
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                alreadyRegistred.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                alreadyRegistred.setCursor(Cursor.getDefaultCursor());
             }
         });
     }
