116b9f723f4cba07f38018417b9fb7d1613c3770
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-01-16 19:31:19 -0300

    feat: implementa generics na classe Reponse, permitindo retorar dados na resposta

--- src/utils/Response.java
+++ src/utils/Response.java
@@ -1,12 +1,20 @@
 package utils;
 
-public class Response {
+public class Response<T> {
 	private final String STATUS;
 	private final String MESSAGE;
+	private final T DATA;
 	
 	public Response(String status, String message) {
 		this.STATUS = status;
 		this.MESSAGE = message;
+		this.DATA = null;
+	}
+	
+	public Response(String status, String message, T data) {
+		this.STATUS = status;
+		this.MESSAGE = message;
+		this.DATA = data;
 	}
 	
 	public String getSTATUS() {
@@ -15,5 +23,9 @@ public class Response {
 
 	public String getMESSAGE() {
 		return MESSAGE;
+	}
+	
+	public T getDATA() {
+		return this.DATA;
 	}
 }
