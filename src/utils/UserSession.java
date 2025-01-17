f65e7cecb179dcb8f266372787f72c6c9de10729
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-01-17 14:24:45 -0300

    feat: adiciona classe de sessão do usuário

---
+++ src/utils/UserSession.java
@@ -0,0 +1,28 @@
+package utils;
+
+import models.User;
+
+public class UserSession {
+	private static UserSession instance;
+	private User user;
+	
+	private UserSession() {}
+	
+	public static UserSession getInstance() {
+		if(instance == null) instance = new UserSession();
+		
+		return instance;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public User getUser() {
+		return this.user;
+	}
+	
+	public void clearSession() {
+		instance = null;
+	}
+}
